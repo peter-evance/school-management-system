9ecbf47246c1912dea34762a0add527e88d19c9a
“Ademic” <adeeyomichael77@gmail.com>
2023-12-04 13:30:13 +0100

    added classroom views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from core.views import SubjectViewSet
+from core.views import ClassRoomViewSet, SubjectViewSet
 
 app_name = 'core'
 
 
 router = routers.DefaultRouter()
 router.register(r'subjects', SubjectViewSet, basename='subjects')
+router.register(r'classrooms', ClassRoomViewSet, basename='classrooms')
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
 ]

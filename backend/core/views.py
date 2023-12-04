8317f1a0a560cc05198c2ed71ffc43b56f784237
“Ademic” <adeeyomichael77@gmail.com>
2023-12-04 13:30:44 +0100

    added classroom viewset

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,15 @@
+from core.models.classroom import ClassRoom
 from core.permissions import IsTeacher
 from core.models.subject import Subject
-from core.serializers import SubjectSerializer
+from core.serializers import *
 from rest_framework import viewsets
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
-    
     serializer_class = SubjectSerializer
+    permission_classes = [IsTeacher]
+
+class ClassRoomViewSet(viewsets.ModelViewSet):
+    queryset = ClassRoom.objects.all()
+    serializer_class = ClassRoomSerializer
     permission_classes = [IsTeacher]

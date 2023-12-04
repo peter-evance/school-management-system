c940b2a5c7eecd2c736d0c8c9309cc20da7f2b24
“Ademic” <adeeyomichael77@gmail.com>
2023-12-04 13:32:31 +0100

    auto assign the class code based on class title

--- backend/core/models/classroom.py
+++ backend/core/models/classroom.py
@@ -3,9 +3,28 @@ from core.choices import *
 
 class ClassRoom(models.Model):
     title = models.CharField(max_length=30, choices=ClassRoomTitleChoices.choices)
-    code = models.CharField(max_length=10, choices=ClassRoomCodeChoices.choices)
+    code = models.CharField(max_length=10, choices=ClassRoomCodeChoices.choices, blank=True,null=True)
     capacity = models.PositiveIntegerField(default=1)
     stream = models.CharField(max_length=1, choices=ClassRoomStreamChoices.choices)
+
+    def update_class_code(self):
+        title_to_code_mapping = {
+            ClassRoomTitleChoices.JUNIOR_SECONDARY_SCHOOL_1: ClassRoomCodeChoices.JSS_1,
+            ClassRoomTitleChoices.JUNIOR_SECONDARY_SCHOOL_2: ClassRoomCodeChoices.JSS_2,
+            ClassRoomTitleChoices.JUNIOR_SECONDARY_SCHOOL_3: ClassRoomCodeChoices.JSS_3,
+            ClassRoomTitleChoices.SENIOR_SECONDARY_SCHOOL_1: ClassRoomCodeChoices.SSS_4,
+            ClassRoomTitleChoices.SENIOR_SECONDARY_SCHOOL_2: ClassRoomCodeChoices.SSS_5,
+            ClassRoomTitleChoices.SENIOR_SECONDARY_SCHOOL_3: ClassRoomCodeChoices.SSS_6,
+        }
+
+        self.code = title_to_code_mapping.get(self.title, self.code)
+        return self.code
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            self.id = self.pk
+        self.code = self.update_class_code()
+        super().save(*args, **kwargs)
+
     
     def __str__(self):
         return self.title

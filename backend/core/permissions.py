2d1c78226f3abfcbf07fb2c206f08a11d05a93c9
“Ademic” <adeeyomichael77@gmail.com>
2023-12-04 13:28:25 +0100

    added more checks to prevent potential errors

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -3,9 +3,21 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
 
+# class IsTeacher(BasePermission):
+#     def has_permission(self, request, view):
+#         teacher = Teacher.objects.filter(user = request.user).first()
+#         if request.user.is_authenticated and teacher:
+#             return True
+#         raise PermissionDenied('You are not allowed to perform this action')
+
 class IsTeacher(BasePermission):
     def has_permission(self, request, view):
-        teacher = Teacher.objects.filter(user = request.user).first()
-        if request.user.is_authenticated and teacher:
-            return True
-        raise PermissionDenied('You are not allowed to perform this action')
+        if request.user.is_authenticated:
+            try:
+                teacher = Teacher.objects.filter(user = request.user).first()
+                if teacher:
+                    return True
+                raise PermissionDenied('You are not allowed to perform this action')
+            except Teacher.DoesNotExist:
+                raise PermissionDenied('You are not allowed to perform this action')
+        raise PermissionDenied('Authentication required for this action')

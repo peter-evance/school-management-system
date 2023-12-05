00e0f9bc35405b2dc615a047634e7369f49c3bac
“Ademic” <adeeyomichael77@gmail.com>
2023-12-05 11:57:05 +0100

    views for other models

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,15 +1,29 @@
-from core.models.classroom import ClassRoom
-from core.permissions import IsTeacher
-from core.models.subject import Subject
+from core.models.student import Student
+from core.permissions import *
 from core.serializers import *
 from rest_framework import viewsets
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-    permission_classes = [IsTeacher]
+    permission_classes = [IsTeacherOrAdmin]
 
 class ClassRoomViewSet(viewsets.ModelViewSet):
     queryset = ClassRoom.objects.all()
     serializer_class = ClassRoomSerializer
-    permission_classes = [IsTeacher]
+    permission_classes = [IsTeacherOrAdmin]
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [IsAdmin]
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [IsTeacherOrAdmin]
+
+class AdminViewSet(viewsets.ModelViewSet):
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer
+    permission_classes = [IsAdmin]

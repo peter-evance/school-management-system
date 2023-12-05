6f26908bcda5da697e01ef7325ced366248ea883
“Ademic” <adeeyomichael77@gmail.com>
2023-12-05 11:58:10 +0100

    Custom user filtered based on role

--- backend/core/models/admin.py
+++ backend/core/models/admin.py
@@ -2,5 +2,5 @@ from django.db import models
 from users.models import CustomUser
 
 class Admin(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 'Admin'})
     created = models.DateTimeField(auto_now_add=True)

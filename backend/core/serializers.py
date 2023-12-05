5f4026d5da640f8fb751143bea81bfeb33b4e390
“Ademic” <adeeyomichael77@gmail.com>
2023-12-05 11:56:34 +0100

    added more serializers(student,admin,teacher)

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,3 +1,6 @@
+from core.models.admin import Admin
+from core.models.student import Student
+from core.models.teacher import Teacher
 from core.models.classroom import ClassRoom
 from core.models.subject import Subject
 from rest_framework import serializers
@@ -11,3 +14,18 @@ class ClassRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassRoom
         fields = ['title', 'code', 'capacity', 'stream']
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ['user', 'classroom', 'created_at', 'assigned_subjects']
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['user', 'classroom', 'address', 'created_at','enrolled_subjects','registration_number']
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = ['user', 'created']

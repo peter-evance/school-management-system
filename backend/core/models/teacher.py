6b5dec9fc47677aa8a73c6bc553fd188f68ddab4
“Ademic” <adeeyomichael77@gmail.com>
2023-12-05 11:59:18 +0100

    Custom user filtered based on role

--- backend/core/models/teacher.py
+++ backend/core/models/teacher.py
@@ -4,7 +4,7 @@ from core.models.subject import Subject
 from core.models.classroom import ClassRoom
 
 class Teacher(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 'Teacher'})
     classroom = models.ForeignKey(ClassRoom, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     assigned_subjects = models.ManyToManyField(Subject, related_name='teachers')

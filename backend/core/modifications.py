8150003da25604fcfded52a4fb8cf645d72b07f2
“Ademic” <adeeyomichael77@gmail.com>
2023-12-05 11:54:23 +0100

    modified:   backend/core/modifications.py

--- backend/core/modifications.py
+++ backend/core/modifications.py
@@ -1,7 +1,8 @@
 """
-curl -H "Authorization: Token 861ed9885fac5bc25d0a98e4b8e9ff925ff56237" http://127.0.0.1:8000/core/subjects/
+curl -H "Authorization: Token bb828cdbf41db326a78c8f51b275353f90e7c9e3" http://127.0.0.1:8000/core/subjects/
 
-curl -X POST -H "Content-Type: application/json" -d '{"title": "English", "code": "ENG"}' http://127.0.0.1:8000/core/subjects/
+curl -X POST -H "Authorization: Token a8cfc8d88057fa5de033dd2a0c3b94b4bbf27c2b" -H "Content-Type: application/json" -d '{"title": "English", "code": "ENG"}' http://127.0.0.1:8000/core/subjects/
+
 
 
 "id": 1,

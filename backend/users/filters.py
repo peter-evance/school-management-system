3ebc3947a22dc8533b56f03274bb804218004703
ademic2022 <adeeyomichael77@gmail.com>
2023-12-22 16:48:34 +0100

    django filter correction

--- backend/users/filters.py
+++ backend/users/filters.py
@@ -1,8 +1,10 @@
 from users.models import CustomUser
 from django_filters import rest_framework as filters
+from django_filters import CharFilter
 
 
 class CustomUserFilter(filters.FilterSet):
+    first_name = CharFilter(lookup_expr='icontains')
     class Meta:
         model = CustomUser
         fields = ["role", "first_name"]

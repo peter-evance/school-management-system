e686ae025aeb8716febdda32dca89d7ccb89ec7d
ademic2022 <adeeyomichael77@gmail.com>
2023-12-27 20:44:45 +0100

    removed duplicate import & segregated exam choice

--- backend/core/models/exams.py
+++ backend/core/models/exams.py
@@ -1,20 +1,12 @@
+from django.db import models
 from datetime import timedelta
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from core.choices import ExamType
 from core.models.subject import Subject
 
 
-from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db import models
-
-
 class Exam(models.Model):
-    class ExamType(models.TextChoices):
-        MIDTERM = "Mid Term"
-        FINAL = "Final Exam"
-        QUIZ = "Quiz"
-
     exam_type = models.CharField(choices=ExamType.choices, max_length=10)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     scheduled_date = models.DateTimeField(auto_now_add=True)

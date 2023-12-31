0c19c1da9202f55f07ca97d4cbeaafdf483e6e73
Peter Evance <peterevance1@gmail.com>
2023-12-31 12:29:14 +0300

    URL config and setting entry

--- backend/school_management_system/settings.py
+++ backend/school_management_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,6 +145,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Media settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
--- backend/school_management_system/urls.py
+++ backend/school_management_system/urls.py
@@ -16,33 +16,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="School Management System API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
+    openapi.Info(
+        title="School Management System API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
 )
-
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth", include("djoser.urls")),
-    path("auth/", include("users.urls", namespace='users')),
-    path("core/", include("core.urls", namespace='core')),
-    
+    path("auth/", include("users.urls", namespace="users")),
+    path("core/", include("core.urls", namespace="core")),
     # Other urls
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

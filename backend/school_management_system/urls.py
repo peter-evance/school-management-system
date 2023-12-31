9f1c796e2c3fcad045f8ec4675d811e8ac5e7408
ademic2022 <adeeyomichael77@gmail.com>
2023-12-31 01:31:20 +0100

    formatted

--- backend/school_management_system/urls.py
+++ backend/school_management_system/urls.py
@@ -21,28 +21,25 @@ from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="School Management System API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
+    openapi.Info(
+        title="School Management System API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
 )
-
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth", include("djoser.urls")),
-    path("auth/", include("users.urls", namespace='users')),
-    path("core/", include("core.urls", namespace='core')),
-    
+    path("auth/", include("users.urls", namespace="users")),
+    path("core/", include("core.urls", namespace="core")),
     # Other urls
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("__debug__/", include("debug_toolbar.urls")),
-
 ]

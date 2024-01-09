abf9c6788fc17433fefffe54465c3ae309a86096
Peter Evance <peterevance1@gmail.com>
2024-01-10 01:28:32 +0300

    Redacted wrong config...

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -106,9 +106,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'OPTIONS': {
-            'deterministic': False,
-            }
     }
 }
 

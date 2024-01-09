28dcdca30f9f28d31bbbf25d804f63fbdeb8bd93
Peter Evance <peterevance1@gmail.com>
2024-01-10 00:21:54 +0300

    Added static files

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Media settings
 MEDIA_URL = "/media/"
--- school_management_system/urls.py
+++ school_management_system/urls.py
@@ -48,3 +48,4 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

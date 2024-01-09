ea2077f22219e82df18c8754289bcfe0cde2fc6b
Peter Evance <peterevance1@gmail.com>
2024-01-10 01:26:22 +0300

    Off

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -106,6 +106,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'deterministic': False,
+            }
     }
 }
 

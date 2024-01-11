b0f63d199878cd14c313ca9c5c67a4c9acfdb399
ademic2022 <adeeyomichael77@gmail.com>
2024-01-11 02:23:51 +0100

    feat: Add docstring for ViewSets

--- core/views.py
+++ core/views.py
@@ -12,6 +12,26 @@ from rest_framework.exceptions import MethodNotAllowed
 
 
 class SubjectViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: SubjectViewSet
+
+    ViewSet for Subject model with CRUD operations and permissions based on user roles.
+
+    Attributes:
+        - queryset: All instances of the Subject model.
+        - serializer_class: Serializer used for Subject model.
+        - filter_backends: Uses DjangoFilterBackend for filtering.
+        - filterset_class: Specifies the filter class to use.
+
+    Methods:
+        - get_permissions(self): Overrides the default permissions based on the action.
+          - For create, destroy, update, and partial_update actions, requires IsTeacherOrAdmin permission.
+          - For other actions, requires IsAuthenticated permission.
+
+    Example Usage:
+        - Manages CRUD operations for Subject model with specific permissions for certain actions.
+    """
+
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
     filter_backends = (filters.DjangoFilterBackend,)
@@ -27,13 +47,53 @@ class SubjectViewSet(ModelViewSet):
 
 
 class ClassRoomViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: ClassRoomViewSet
+
+    ViewSet for ClassRoom model with CRUD operations and permissions for teachers and admins.
+
+    Attributes:
+        - queryset: All instances of the ClassRoom model.
+        - serializer_class: Serializer used for ClassRoom model.
+        - permission_classes: Requires IsTeacherOrAdmin permission for all actions.
+
+    Example Usage:
+        - Manages CRUD operations for ClassRoom model with permissions restricted to teachers and admins.
+    """
+
     queryset = ClassRoom.objects.all()
     serializer_class = ClassRoomSerializer
     permission_classes = [IsTeacherOrAdmin]
 
 
 class TeacherViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: TeacherViewSet
+
+    ViewSet for Teacher model with CRUD operations, custom actions, and permissions based on user roles.
+
+    Attributes:
+        - queryset: All instances of the Teacher model.
+
+    Methods:
+        - get_serializer_class(self): Determines the serializer class based on the action.
+          - Uses TeacherSerializer for list and retrieve actions, otherwise uses TeacherSerializer2.
+        - get_permissions(self): Overrides the default permissions based on the action.
+          - For create, destroy, partial_update, and update actions, requires IsAdmin permission.
+          - For other actions, requires IsAuthenticated permission.
+
+    Custom Actions:
+        - get_assigned_subjects(self, request): Retrieves and returns subjects assigned to the teacher.
+          - Requires the teacher to be authenticated and associated with a user.
+          - Returns a 404 response if the teacher or subjects are not found.
+          - Uses SubjectSerializer to serialize and return the assigned subjects.
+
+    Example Usage:
+        - Manages CRUD operations for Teacher model with custom actions and specific permissions.
+    """
+
     queryset = Teacher.objects.all()
+
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
             return TeacherSerializer
@@ -64,15 +124,42 @@ class TeacherViewSet(ModelViewSet):
 
         return Response(serializer.data, status=200)
 
-
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
 
+
 class StudentViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: StudentViewSet
+
+    ViewSet for Student model with CRUD operations, custom actions, filtering, and permissions based on user roles.
+
+    Attributes:
+        - queryset: All instances of the Student model.
+        - filter_backends: Uses DjangoFilterBackend for filtering.
+        - filterset_class: Specifies the filter class to use.
+
+    Methods:
+        - get_serializer_class(self): Determines the serializer class based on the action.
+          - Uses StudentSerializer for list and retrieve actions, otherwise uses StudentSerializer2.
+        - get_permissions(self): Overrides the default permissions based on the action.
+          - For create, destroy, update, and partial_update actions, requires IsTeacherOrAdmin permission.
+          - For other actions, requires IsAuthenticated permission.
+
+    Custom Actions:
+        - get_enrolled_subjects(self, request): Retrieves and returns subjects enrolled by the student.
+          - Requires the student to be authenticated and associated with a user.
+          - Returns a 404 response if the student or subjects are not found.
+          - Uses SubjectSerializer to serialize and return the enrolled subjects.
+
+    Example Usage:
+        - Manages CRUD operations for Student model with custom actions, filtering, and specific permissions.
+    """
+
     queryset = Student.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = StudentFilter
-    # permission_classes = [IsTeacherOrAdmin]
+
     def get_permissions(self):
         if self.action in ["create", "destroy", "update", "partial_update"]:
             permission_classes = [IsTeacherOrAdmin]
@@ -80,7 +167,7 @@ class StudentViewSet(ModelViewSet):
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
-    
+
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
             return StudentSerializer
@@ -105,12 +192,46 @@ class StudentViewSet(ModelViewSet):
 
 
 class AdminViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: AdminViewSet
+
+    ViewSet for Admin model with CRUD operations and permissions restricted to admin users.
+
+    Attributes:
+        - queryset: All instances of the Admin model.
+        - serializer_class: Serializer used for Admin model.
+        - permission_classes: Requires IsAdmin permission for all actions.
+
+    Example Usage:
+        - Manages CRUD operations for Admin model with permissions restricted to admin users.
+    """
+
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
     permission_classes = [IsAdmin]
 
 
 class ExamViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: ExamViewSet
+
+    ViewSet for Exam model with CRUD operations and permissions for teachers and admins.
+
+    Attributes:
+        - queryset: All instances of the Exam model.
+        - serializer_class: Serializer used for Exam model.
+        - permission_classes: Requires IsTeacherOrAdmin permission for all actions.
+
+    Custom List Action:
+        - list(self, request, *args, **kwargs): Overrides the default list action.
+          - Checks if the queryset is empty and provides appropriate responses.
+            - If query parameters are provided but no matches are found, returns a 404 response.
+            - If no query parameters are provided and no exams are in the database, returns a 200 response.
+
+    Example Usage:
+        - Manages CRUD operations for Exam model with specific permissions and custom list action.
+    """
+
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
     permission_classes = [IsTeacherOrAdmin]
@@ -138,6 +259,26 @@ class ExamViewSet(ModelViewSet):
 
 
 class SubjectResultViewSet(ModelViewSet):
+    """
+    Django ModelViewSet: SubjectResultViewSet
+
+    ViewSet for SubjectResult model with CRUD operations and permissions for teachers and admins.
+
+    Attributes:
+        - queryset: All instances of the SubjectResult model.
+        - serializer_class: Serializer used for SubjectResult model.
+        - permission_classes: Requires IsTeacherOrAdmin permission for all actions.
+
+    Custom List Action:
+        - list(self, request, *args, **kwargs): Overrides the default list action.
+          - Checks if the queryset is empty and provides appropriate responses.
+            - If query parameters are provided but no matches are found, returns a 404 response.
+            - If no query parameters are provided and no subject results are in the database, returns a 200 response.
+
+    Example Usage:
+        - Manages CRUD operations for SubjectResult model with specific permissions and custom list action.
+    """
+
     queryset = SubjectResult.objects.all()
     serializer_class = SubjectResultSerializer
     permission_classes = [IsTeacherOrAdmin]

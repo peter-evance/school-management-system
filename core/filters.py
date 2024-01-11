b5baa97d09bc75dc90de9bcf987b266d662b2fab
ademic2022 <adeeyomichael77@gmail.com>
2024-01-11 02:21:44 +0100

    feat: Write docstring for filter classes

--- core/filters.py
+++ core/filters.py
@@ -6,6 +6,11 @@ from django_filters import rest_framework as filters
 
 
 class StudentFilter(filters.FilterSet):
+    """
+     StudentFilter:
+    - Filters students based on user information using a case-insensitive match.
+    """
+
     user = filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
@@ -14,18 +19,33 @@ class StudentFilter(filters.FilterSet):
 
 
 class SubjectFilter(filters.FilterSet):
+    """
+     SubjectFilter:
+    - Filters subjects based on the associated class room.
+    """
+
     class Meta:
         model = Subject
         fields = ["class_room"]
 
 
 class ExamFilter(filters.FilterSet):
+    """
+     ExamFilter:
+    - Filters exams based on exam type and associated subject.
+    """
+
     class Meta:
         model = Exam
         fields = ("exam_type", "subject")
 
 
 class SubjectResultFilter(filters.FilterSet):
+    """
+     SubjectResultFilter:
+    - Filters subject results based on the associated student and exam.
+    """
+
     class Meta:
         model = SubjectResult
         fields = ("student", "exam")

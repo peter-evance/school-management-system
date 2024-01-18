c6923a4c5fbb1f59f6689589145a9b7549f359cb
ademic2022 <adeeyomichael77@gmail.com>
2024-01-18 19:28:45 +0100

    test: correct test cases to adapt to updated endpoints

--- tests/users/tests/test_models.py
+++ tests/users/tests/test_models.py
@@ -3,19 +3,20 @@ import pytest
 from users.serializers import CustomUserCreateSerializer
 
 
-
 class TestCreateUser:
     @pytest.mark.django_db
     def test_create_user(self):
-        serializer = CustomUserCreateSerializer(data={
-            'username':'michaelademic',
-            'first_name': 'michael',
-            'last_name': 'ademic',
-            'sex': 'Male',
-            'role': 'Teacher',
-            'password': '12345678QQ'
-
-        })
+        serializer = CustomUserCreateSerializer(
+            data={
+                "username": "michaelademic",
+                "first_name": "michael",
+                "last_name": "ademic",
+                "sex": "Male",
+                "role": "Teacher",
+                "password": "12345678QQ",
+                "re_password": "12345678QQ",
+            }
+        )
 
         if serializer.is_valid():
             user = serializer.save()

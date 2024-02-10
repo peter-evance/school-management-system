de874872703e743d6aa4cbea650308ec4b535bd4
“Ademic” <adeeyomichael77@gmail.com>
2024-02-10 13:40:41 +0100

    feat: add notify_admins_on_registration method to notify admins when a new user registers.

--- users/models.py
+++ users/models.py
@@ -40,35 +40,89 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=10, choices=RoleChoices.choices)
     is_approved = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ["first_name", "last_name", "sex", "role", "username", "is_approved", "address"]
-    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "sex",
+        "role",
+        "username",
+        "is_approved",
+        "address",
+    ]
+    USERNAME_FIELD = "email"
 
     @property
     def get_full_name(self):
         """Return the full name"""
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
 
     def get_role(self):
         return self.get_role_display()
 
     def get_users_by_role(self, role):
         return CustomUser.objects.filter(role=role)
-    
+
     def approve_user(self):
         self.is_approved = True
         self.save()
         self.send_approval_email()
-        
+
     def send_approval_email(self):
-        subject = 'Your account has been approved'
-        template = 'approval_email.html'
-        context = {'recipient_name': self.first_name.capitalize()}
+        try:
+            subject = "Your account has been approved"
+            template = "approval_email.html"
+            context = {"recipient_name": self.first_name.capitalize()}
 
-        html_message = render_to_string(template, context)
-        from_email = settings.DEFAULT_FROM_EMAIL
-        recipient_list = [self.email]
+            html_message = render_to_string(template, context)
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [self.email]
 
-        send_mail(subject, None, from_email, recipient_list, html_message=html_message)
+            send_mail(
+                subject, None, from_email, recipient_list, html_message=html_message
+            )
+
+            print(f"Approval email sent to {self.email}.")
+        except Exception as e:
+            print(f"Failed to send approval email to {self.email}: {e}")
+
+    def notify_admins_on_registration(self):
+        try:
+            # Filter all admin users except the current user
+            admins = CustomUser.objects.filter(
+                role=self.RoleChoices.ADMIN, is_approved=True
+            ).exclude(id=self.id)
+            admin_emails = admins.values_list("email", flat=True)
+
+            # Create notifications for each admin
+            for admin in admins:
+                Notification.objects.create(
+                    user=admin,
+                    subject=f"New User Registration: {self.get_full_name}",
+                    message=f"A new user with username '{self.username}' has registered on the platform.",
+                )
+
+            subject = f"New User Registration: {self.username}"
+            template = "registration_notification.html"
+            from_email = settings.DEFAULT_FROM_EMAIL
+            context = {
+                "recipient_name": self.get_full_name,
+                "email": self.email,
+                "user_role": self.role,
+            }
+
+            html_message = render_to_string(template, context)
+            # Send email to admin users
+            send_mail(
+                subject,
+                None,
+                from_email,
+                admin_emails,
+                html_message=html_message,
+            )
+
+            # print(f"Notification email sent to {len(admin_emails)} admins.")
+        except Exception as e:
+            print(f"Failed to send registration notification email: {e}")
 
     def __str__(self):
         return self.get_full_name
@@ -83,3 +137,18 @@ class ProfileImage(models.Model):
 
     def __str__(self):
         return f"Profile Image for {self.user.username}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def mark_as_read(self):
+        self.read = True
+        self.save()
